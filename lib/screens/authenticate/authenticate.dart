278179e2056377dd2d948832886f17a434e9ab54
Eric Walther <62553571+EricWaltherHH@users.noreply.github.com>
2020-03-24 03:20:00 +0100

    Text geändert

--- lib/screens/authenticate/authenticate.dart
+++ lib/screens/authenticate/authenticate.dart
@@ -10,7 +10,8 @@ class _AuthenticateState extends State<Authenticate> {
   @override
   Widget build(BuildContext context) {
     return Container(
-    child: SignIn(),  
+    child: SignIn(),
+    text: Text('Successfully'),
     );
   }
 }
